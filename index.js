2499691ea2d1afbaac39b46de0012b50b22e5740
Vinicius <vianaxd99@gmail.com>
2022-08-25 22:55:58 -0300

    get 10 tweets done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ server.use(cors());
 server.use(express.json());
 
 const users = [];
+const tweets = [];
 
 server.post("/sign-up", (req, res) => {
     const newUser = req.body;
@@ -13,5 +14,35 @@ server.post("/sign-up", (req, res) => {
     res.send("OK");
 });
 
+server.get("/tweets", (req, res) => {
+    const visibleTweets = getVisibleTweets();
+
+    res.send(visibleTweets);
+});
+
 
 server.listen(5000);
+
+function getAvatar(username) {
+    const user = users.find(value => value.username === username);
+
+    return user.avatar;
+}
+
+function getVisibleTweets() {
+    const visibleTweets = [];
+
+    if (tweets.length > 0) {
+        for (let i = tweets.length - 1; (i >= tweets.length - 10 && i >= 0); i--) {
+            const value = tweets[i];
+
+            visibleTweets.push({
+                username: value.username,
+                avatar: getAvatar(value.username),
+                tweet: value.tweet
+            });
+        }
+    }
+
+    return visibleTweets;
+}
