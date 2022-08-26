0b379e0deaa095bce0b9b4f7ee5cb92779a298c4
Vinicius <vianaxd99@gmail.com>
2022-08-25 22:18:15 -0300

    user post done

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
+server.use(express.json());
 
+const users = [];
 
+server.post("/sign-up", (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.send("OK");
+});
 
 
 server.listen(5000);
